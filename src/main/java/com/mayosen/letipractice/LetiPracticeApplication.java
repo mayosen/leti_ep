5bc2e256c6915dc69726c2012924608da3398947
mayosen <gare577@gmail.com>
2023-04-22 00:33:58 +0300

    Init Gradle project. Describe it in README

---
+++ src/main/java/com/mayosen/letipractice/LetiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mayosen.letipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetiPracticeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mayosen/letipractice/services/OcrService.java
@@ -0,0 +1,7 @@
+package com.mayosen.letipractice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OcrService {
+}
